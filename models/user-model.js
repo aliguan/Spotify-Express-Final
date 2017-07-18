a7f5e38ff9a82554dd102ff1980b00adaf9d6dae
Alice Guan <aliguan726@gmail.com>
2017-07-18 19:23:15 -0400

    Fix User Model to (un)accomodate images

--- models/user-model.js
+++ models/user-model.js
@@ -3,16 +3,18 @@ const Schema   = mongoose.Schema;
 
 const UserSchema = Schema({
     //synonymous to spotify user model
+    firstname: String, 
+    lastname: String,
     country: String,
     display_name: String,
     email: String,
     href: String,
     id: String,
-    images:
-        [ {
-           url: String
-         }
-        ],
+    // images:
+    //     [ {
+    //        url: { type: String, default: '../public/images/avatar.png' }
+    //      }
+    //     ],
     type: String,
     uri: String,
     tracks: String
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,39 +7,39 @@ const User                 = require('../models/user-model.js');
 const userRoutes           = express.Router();
 
 userRoutes.post('/newUser', (req, res, next) => {
+
     const newUser = new User ({
         country: req.body.country,
         display_name: req.body.display_name,
         email: req.body.email,
         href: req.body.href,
         id: req.body.id,
-        images:
-            [{
-               url: req.body.images[0].url,
-             }
-            ],
+        // images:
+        //     [{
+        //        url: req.body.images[0].url,
+        //      }
+        //     ],
         type: req.body.type,
         uri: req.body.uri,
         tracks: 'hi',
     });
-
+    console.log('new user made');
     //Prevent Mulitple users from being created
 
     User.findOne({ 'email': `${req.body.email}` }, (err, user) => {
-        if(err) { return res.status(500).json(err) };
+        if(err) { console.log('err from findone'); return res.status(500).json(err) };
         if( user ) {
             console.log( user + ' was found' );
         } else {
-            console.log('you suck');
             newUser.save( (err) => {
-                if (err)             { return res.status(500).json(err) }
+                console.log('trying to save');
+                if (err)            { return res.status(500).json(err) }
                 if (newUser.errors) { return res.status(400).json(newUser) }
+                return true;
             });
         }
 
     });
-
-
 
 });
 
