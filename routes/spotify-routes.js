e8db80a029cdf05dd449fdc7d8e14da2d44d5e1d
Alice Guan <aliguan726@gmail.com>
2017-07-29 09:33:54 -0400

    dfgs

--- routes/spotify-routes.js
+++ routes/spotify-routes.js
@@ -96,10 +96,9 @@ spotifyAuthRoutes.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.json({
+        res.send({
             access_token: access_token,
             refresh_token: refresh_token,
-            why: 'why!'
             });
           } else {
             res.redirect('/#' +
