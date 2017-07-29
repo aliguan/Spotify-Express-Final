ba6705a2cfdc3b85457ee134e4991f2da05c9bec
Alice Guan <aliguan726@gmail.com>
2017-07-29 00:06:08 -0400

    :((

--- routes/spotify-routes.js
+++ routes/spotify-routes.js
@@ -96,6 +96,7 @@ spotifyAuthRoutes.get('/callback', function(req, res) {
         res.json({
             access_token: access_token,
             refresh_token: refresh_token
+            why: 'why!'
             });
           } else {
             res.redirect('/#' +
