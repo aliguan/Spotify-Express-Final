ff489bee116bd083c5aa6bf84f391fdd9211c098
Alice Guan <aliguan726@gmail.com>
2017-07-28 22:58:52 -0400

    Chnage callback uri

--- routes/spotify-routes.js
+++ routes/spotify-routes.js
@@ -7,7 +7,7 @@ const spotifyAuthRoutes   = express.Router();
 
 var client_id = '171a4c5c858c492f838a99535c3d3851'; // Your client id
 var client_secret = '354d93dcc82a42d2ba12064ff8a2eb65';// Your secret
-var redirect_uri = 'http://localhost:4200/callback'; // Your redirect uri
+var redirect_uri = 'https://spotifriend.herokuapp.com/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
