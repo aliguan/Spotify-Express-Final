8acb3af4050bebb0bea04e554257918ef2eb6ed2
Alice Guan <aliguan726@gmail.com>
2017-07-30 19:41:09 -0400

    Display correct percentages

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ dotenv.config();
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/spotifydb');
 // mongoose.connect('mongodb://localhost/spotifydb');
-// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb://localhost/spotifydb');
+mongoose.connect(process.env.MONGODB_URI);
 
 var mongoDB = mongoose.connection;
 
